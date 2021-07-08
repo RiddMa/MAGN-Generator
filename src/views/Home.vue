2e16c4c3e2b2832b409466604261ad6d9dac564b
RiddMa <riddma@qq.com>
2021-07-08 17:01:31 +0800

    one last try

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,9 +8,10 @@
     <!--    </div>-->
     <router-view></router-view>
     <Settings></Settings>
+
     <Review v-if="!fitPhone"></Review>
     <ReviewPhone v-if="fitPhone"></ReviewPhone>
-
+    <a-button @click="scrollAndCapture">Draw Canvas</a-button>
     <div class="customFooter">Designed by Ridd.</div>
   </div>
 </template>
@@ -58,7 +59,45 @@ export default {
       console.log(document.body.clientWidth);
     }, 0);
   },
-  methods: {},
+  methods: {
+    async scrollAndCapture() {
+      await this.toTop(window, () => this.capture());
+    },
+    toTop(element = window, callback) {
+      element.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+      });
+      if (!callback) return;
+      if (element === window ? element.scrollY === 0 : element.scrollTop === 0)
+        return callback();
+      let running = function () {
+        let top = this === window ? this.scrollY : this.scrollTop;
+        if (top === 0) {
+          this.removeEventListener("scroll", running);
+          return callback();
+        }
+      };
+      element.addEventListener("scroll", running, false);
+    },
+    async capture() {
+      await this.updateComment();
+      await this.$store.commit("setCommentInput", false);
+      const targetDom = document.getElementById("toImage");
+      html2canvas(targetDom, {
+        width: targetDom.clientWidth,
+        height: targetDom.clientHeight,
+        scrollX: 0,
+        scrollY: 0,
+        allowTaint: true,
+        useCORS: true,
+      }).then(function (canvas) {
+        document.body.appendChild(canvas);
+      });
+      await this.$store.commit("setCommentInput", true);
+    },
+  },
 };
 </script>
 
