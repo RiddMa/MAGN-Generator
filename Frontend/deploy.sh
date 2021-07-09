3812a1c35c30e1f623afa4f191cd9eb4a897d723
RiddMa <riddma@qq.com>
2021-07-09 19:05:09 +0800

    修正部署脚本

--- Frontend/deploy.sh
+++ Frontend/deploy.sh
@@ -3,6 +3,8 @@ cd ~/MAGN-Generator &&
 git fetch --all &&
 git reset --hard origin/master &&
 git pull &&
+npm install &&
+cd ~/MAGN-Generator/Frontend &&
 npm run build &&
 rm -rf /usr/share/nginx/html/* &&
 cp -r ~/MAGN-Generator/dist/* /usr/share/nginx/html &&
