001f251b678bd1d84b3ae698cdddc4c454508835
RiddMa <riddma@qq.com>
2021-07-09 19:13:09 +0800

    再修正部署脚本...

--- Frontend/deploy.sh
+++ Frontend/deploy.sh
@@ -7,6 +7,6 @@ npm install &&
 cd ~/MAGN-Generator/Frontend &&
 npm run build &&
 rm -rf /usr/share/nginx/html/* &&
-cp -r ~/MAGN-Generator/dist/* /usr/share/nginx/html &&
+cp -r ./dist/* /usr/share/nginx/html &&
 nginx -s reload &&
 echo "Deployed"
