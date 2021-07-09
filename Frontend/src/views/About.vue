0aa19a5b56d020647c95cf9b07fda3181601082b
RiddMa <riddma@qq.com>
2021-07-09 21:02:22 +0800

    关于页样式优化

--- Frontend/src/views/About.vue
+++ Frontend/src/views/About.vue
@@ -3,7 +3,7 @@
     <a-row class="toImage" id="toImage" ref="toImage">
       <a-col flex="auto"></a-col>
       <a-col :xxl="14" :xl="16" :lg="18" :md="20" :sm="22" :xs="24">
-        <a-card>
+        <a-card class="aboutCard">
           <a-typography-title class="awesomeTitle">
             MAG(N) 影评生成器
           </a-typography-title>
@@ -25,17 +25,14 @@
             >使用指南
           </a-typography-title>
           <a-typography-paragraph class="aboutText">
-            前往“Home”页面，在“选项”中填写影片英文标题、中文标题、年份，并在下方勾选影片所属类型。（影片分类参考了
+            前往“Home”页面。<br/>在“选项”中填写影片英文标题、中文标题、年份，并在下方勾选影片所属类型。（影片分类参考了
             <a-typography-link
               href="https://en.wikipedia.org/wiki/Film_genre"
               rel="noopener"
               target="_blank"
               >维基百科
             </a-typography-link>
-            ），点击“清空全部”按钮可以清空关于影片的所有信息（包括影片本身信息及打分、评论）。
-          </a-typography-paragraph>
-          <a-typography-paragraph class="aboutText">
-            在下方卡片，可以预览生成海报图片样式。从四个维度，点击星星来进行打分（评分维度参考了学院奖奖项设置）。在最下方输入框，输入影评内容。
+            ）<br/>点击“清空全部”按钮可以清空关于影片的所有信息。（包括影片本身信息及打分、评论）<br/>在下方卡片，可以预览生成海报图片样式。<br/>从四个维度，点击星星来进行打分（评分维度参考了学院奖奖项设置）。<br/>在最下方输入框，输入影评内容。
           </a-typography-paragraph>
           <a-typography-title class="aboutTitle" :level="2"
             >相关链接
@@ -59,6 +56,10 @@
 </template>
 <script></script>
 <style scoped>
+.aboutCard{
+  margin: 2em 4vw;
+  border-color: rgba(66, 185, 131, 0.5);
+}
 .awesomeTitle {
   text-align: start;
   vertical-align: center;
