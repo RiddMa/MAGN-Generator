79ddfe4213d775011a51a5164eaf0e327e60eac4
RiddMa <riddma@qq.com>
2021-08-02 00:54:44 +0800

    v2.0.2.1 PWA测试2

--- app_vuetify/src/registerServiceWorker.js
+++ app_vuetify/src/registerServiceWorker.js
@@ -19,13 +19,13 @@ if (process.env.NODE_ENV === "production") {
       console.log("Content has been cached for offline use.");
     },
     updatefound() {
-      // store.commit("showToast", {
-      //   dialog: true,
-      //   type: "updatePage",
-      //   message: "应用有更新，正在加载并更新页面",
-      // });
-      alert("应用有更新，正在加载并更新页面");
       console.log("New content is downloading.");
+      store.commit("showToast", {
+        dialog: true,
+        type: "updatePage",
+        message: "应用有更新，正在加载并更新页面",
+      });
+      // alert("应用有更新，正在加载并更新页面");
     },
     updated() {
       console.log("New content is available; please refresh.");
