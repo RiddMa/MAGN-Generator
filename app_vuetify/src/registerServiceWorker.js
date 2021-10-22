28a143dc38125efc2ae239fa783d9f077ba1b7b6
RiddMa <riddma@qq.com>
2021-10-22 21:47:01 +0800

    tweak serviceworker again

--- app_vuetify/src/registerServiceWorker.js
+++ app_vuetify/src/registerServiceWorker.js
@@ -19,19 +19,19 @@ if (process.env.NODE_ENV === "production") {
     },
     updatefound() {
       console.log("New content is downloading.");
-      store.commit("showToast", {
-        dialog: true,
-        type: "updatePage",
-        message: "应用有更新，请刷新页面",
-      });
     },
-    async updated(registration) {
+    async updated() {
       console.log("New content is available; please refresh.");
-      await registration.update();
+      // await registration.update();
       // caches.keys().then(function (names) {
       //   for (let name of names) caches.delete(name);
       // });
-      // window.location.reload(true);
+      store.commit("showToast", {
+        dialog: true,
+        type: "updatePage",
+        message: "应用已更新，正在刷新页面",
+      });
+      window.location.reload(true);
     },
     offline() {
       console.log("No internet connection found. App is running in offline mode.");
