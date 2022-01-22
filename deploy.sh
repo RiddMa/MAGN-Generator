97833bbc732f98071992f809e7fff7588b917671
RiddMa <riddma@qq.com>
2022-01-22 22:59:57 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ git pull &&
 cd ~/MAGN-Generator/app_vuetify &&
 npm install &&
 npm run build &&
-rm -rf /usr/share/nginx/html/* &&
-cp -r ./dist/* /usr/share/nginx/html &&
+rm -rf /usr/share/nginx/magn/* &&
+cp -r ./dist/* /usr/share/nginx/magn &&
 nginx -s reload &&
 echo "Deployed"
